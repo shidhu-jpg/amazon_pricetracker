b776c2bcca3a2c7c3b8f3d8366fc815390e18e40
shidhu-jpg <mlshidhu@gmail.com>
2025-11-22 05:48:07 +0530

    Update email credentials and message details

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import smtplib
 from bs4 import BeautifulSoup
 from email.message import EmailMessage
 
-app_password_person_shidhu = "llexhgwyodtzsraf"
+app_password_person_shidhu = " Your app passwodr"
 
 header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
@@ -19,8 +19,8 @@ price = int(price)
 if price<100:
     msg = EmailMessage()
     msg['Subject'] = f"product Deal Alert: Price:{price}"
-    msg['From'] = "mlshidhu@gmaill.com"
-    msg['To'] = "personshidhu@gmail.com"
+    msg['From'] = "shidhu@gmaill.com"
+    msg['To'] = "pshidhu@gmail.com"
     msg.set_content(f"Product price droped to  at just {price}")
 
     with smtplib.SMTP('smtp.gmail.com',587) as smtp:
